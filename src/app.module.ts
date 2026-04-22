2b876c4f0f4418bf364a459308e1aea2e0650408
Lucas Gauto <lucas.gauto@compasso.com.br>
2026-04-22 01:29:40 -0300

    feat: prisma, auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
